7de06e517286ccb0e131c3199b0c789a45d512a3
Adrian Rojas <arojasgomez.93@gmail.com>
2024-11-16 14:07:03 -0300

    Main background elements

--- CMM_Timber/src/Program/Game.cpp
+++ CMM_Timber/src/Program/Game.cpp
@@ -1,9 +1,95 @@
+#include <iostream>
 #include "Game.h"
 
 Game::Game()
 {
+	videoMode = new sf::VideoMode(SCREEN_X, SCREEN_Y);
+	window = new sf::RenderWindow(*videoMode, windowName);
+	event = new sf::Event;
+	SetupSprites();
+
+
+
 }
 
 Game::~Game()
 {
+	if (videoMode != nullptr)
+	{
+		delete videoMode;
+		videoMode = nullptr;
+	}
+
+	if (window != nullptr)
+	{
+		delete window;
+		window = nullptr;
+	}
+
+	if (event != nullptr)
+	{
+		delete event;
+		event = nullptr;
+	}
+}
+
+void Game::Run()
+{
+
+	while (window->isOpen())
+	{
+		while (window->pollEvent(*event))
+		{
+			if (event->type == sf::Event::Closed)
+				window->close();
+
+		}
+
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Escape))
+			window->close();
+
+
+		window->draw(spriteBackground);
+		window->draw(spriteCloud01);
+		window->draw(spriteCloud02);
+		window->draw(spriteCloud03);
+		window->draw(spriteTree);
+		window->draw(spriteBee);
+		window->display();
+	}
+
+}
+
+void Game::SetupSprites()
+{
+	if (textureBackground.loadFromFile(pathBackgroundGraphic))
+	{
+		spriteBackground.setTexture(textureBackground);
+		spriteBackground.setPosition(0, 0);
+	}
+	else
+		std::cout << "ERROR LOADING BACKGROUND TEXTURE\n";
+
+	if (textureTree.loadFromFile(pathTreeGraphic))
+	{
+		spriteTree.setTexture(textureTree);
+		spriteTree.setPosition(TREE_X, TREE_Y);
+	}
+
+	if (textureBee.loadFromFile(pathBeeGraphic))
+	{
+		spriteBee.setTexture(textureBee);
+		spriteBee.setPosition(0, 800);
+	}
+
+	if (textureCloud.loadFromFile(pathCloudGraphic))
+	{
+		spriteCloud01.setTexture(textureCloud);
+		spriteCloud02.setTexture(textureCloud);
+		spriteCloud03.setTexture(textureCloud);
+		spriteCloud01.setPosition(0, 0);
+		spriteCloud02.setPosition(0, 250);
+		spriteCloud03.setPosition(0, 500);
+	}
+
 }
--- CMM_Timber/src/Program/Game.h
+++ CMM_Timber/src/Program/Game.h
@@ -1,12 +1,57 @@
 #pragma once
+#include <SFML/Graphics.hpp>
 
 class Game
 {
 public:
 	Game();
 	~Game();
+	void Run();
 
 private:
+
+	const int SCREEN_X = 1920;
+	const int SCREEN_Y = 1080;
+	const int TREE_X = 810;
+	const int TREE_Y = 0;
+	const std::string windowName = "Timber!!!";
+	const std::string pathBackgroundGraphic = 
+		"res/assets/graphics/background.png";
+	const std::string pathTreeGraphic = 
+		"res/assets/graphics/tree.png";
+	const std::string pathBeeGraphic =
+		"res/assets/graphics/bee.png";
+	const std::string pathCloudGraphic =
+		"res/assets/graphics/cloud.png";
+
+	sf::RenderWindow* window;
+	sf::VideoMode* videoMode;
+	sf::Event* event;
+
+
+	sf::Texture textureBackground;
+	sf::Sprite spriteBackground;
+
+	sf::Texture textureTree;
+	sf::Sprite spriteTree;
+
+	sf::Texture textureBee;
+	sf::Sprite spriteBee;
+	bool isbeeActive = false;
+	float beeSpeed = 0.0f;
+
+	sf::Texture textureCloud;
+	sf::Sprite spriteCloud01;
+	bool isCloud01Active = false;
+	float Cloud01Speed = 0.0f;
+	sf::Sprite spriteCloud02;
+	bool isCloud02Active = false;
+	float Cloud02Speed = 0.0f;
+	sf::Sprite spriteCloud03;
+	bool isCloud03Active = false;
+	float Cloud03Speed = 0.0f;
+
+	void SetupSprites();
 
 };
 
