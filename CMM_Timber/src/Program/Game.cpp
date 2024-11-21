a013cbc8ec8134b98db7187a4c1ead6eb1a99070
Adrian Rojas <arojasgomez.93@gmail.com>
2024-11-21 06:46:43 -0300

    core completed

--- CMM_Timber/src/Program/Game.cpp
+++ CMM_Timber/src/Program/Game.cpp
@@ -7,16 +7,34 @@ Game::Game()
 	videoMode = new sf::VideoMode(SCREEN_X, SCREEN_Y);
 	window = new sf::RenderWindow(*videoMode, windowName);
 	event = new sf::Event;
+
 	SetupSprite(pathBackgroundGraphic, textureBackground, 0.0f, 0.0f, spriteBackground);
+	SetupSprite(pathTreeGraphic, textureTree, 810.0f, 0.0f, spriteTree);
 	SetupSprite(pathBeeGraphic, textureBee, 0.0f, 800.0f, spriteBee);
 	SetupSprite(pathCloudGraphic, textureCloud, 0.0f, 0.0f, spriteCloud01);
 	SetupSprite(pathCloudGraphic, textureCloud, 0.0f, 250, spriteCloud02);
 	SetupSprite(pathCloudGraphic, textureCloud, 0.0f, 500, spriteCloud03);
-	SetupText(pathFont, font, messageText, messageString, sizeWelcomeText);
+	SetupSprite(pathPlayerGraphic, texturePlayer, playerInitPosX, playerInitPosY, spritePlayer);
+	SetupSprite(pathRIPGraphic, textureRIP, RIPInitPosX, RIPInitPosY, spriteRIP);
+	SetupSprite(pathAxeGraphic, textureAxe, axeInitPosX, axeInitPosY, spriteAxe);
+	SetupSprite(pathLogGraphic, textureLog, logInitPosX, logInitPosY, spriteLog);
+
+
+
+	SetupText(pathFont, font, messageText, messageString, sizeMessageText);
 	SetupText(pathFont, font, scoreText, STRING_SCORE_TEXT, sizeScoreText);
+
 	PositionText(messageText, posMessageX, posMessageY);
 	PositionText(scoreText, posScoreX, posScoreY);
+
 	CreateBar(timeBar, timeBarStartWidth, timeBarHeight);
+
+	for (int i = 0; i < NUM_BRANCHES; i++)
+	{
+		SetupSprite(pathBranchGraphic, textureBranch, -2000, -2000, branches[i]);
+		branches[i].setOrigin(220, 20);
+	}
+
 }
 
 Game::~Game()
@@ -42,10 +60,24 @@ Game::~Game()
 
 void Game::Run()
 {
+	UpdateBranches(1);
+	UpdateBranches(2);
+	UpdateBranches(3);
+	UpdateBranches(4);
+	UpdateBranches(5);
+
 	while (window->isOpen())
 	{
 		while (window->pollEvent(*event))
 		{
+
+			if (event->type == sf::Event::KeyReleased && !isPaused)
+			{
+				canAcceptInput = true;
+
+				spriteAxe.setPosition(2000, axeInitPosY);
+			}
+
 			if (event->type == sf::Event::Closed)
 				window->close();
 
@@ -55,9 +87,51 @@ void Game::Run()
 			if (sf::Keyboard::isKeyPressed(sf::Keyboard::Enter))
 			{
 				isPaused = false;
-				timeRemaining = 6.0f;
+				timeRemaining = INITIAL_TIME;
 				dtClock.restart();
+				spriteRIP.setPosition(RIPInitPosX, RIPInitPosY);
 
+				for (int i = 0; i < NUM_BRANCHES; i++)
+				{
+					branchPositions[i] = Side::NONE;
+				}
+				canAcceptInput = true;
+
+			}
+
+			if (canAcceptInput)
+			{
+				if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right))
+				{
+					playerSide = Side::RIGHT;
+					score++;
+					timeRemaining += (2 / score) + .15;
+					spriteAxe.setPosition(AXE_POSITION_RIGHT, axeInitPosY);
+					spritePlayer.setPosition(1200, playerInitPosY);
+
+					UpdateBranches(score);
+					spriteLog.setPosition(logInitPosX, logInitPosY);
+					logSpeedX = -5000;
+					isLogActive = true;
+
+					canAcceptInput = false;
+				}
+
+				if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left))
+				{
+					playerSide = Side::LEFT;
+					score++;
+					timeRemaining += (2 / score) + .15;
+					spriteAxe.setPosition(AXE_POSITION_LEFT, axeInitPosY);
+					spritePlayer.setPosition(580, playerInitPosY);
+
+					UpdateBranches(score);
+					spriteLog.setPosition(logInitPosX, logInitPosY);
+					logSpeedX = 5000;
+					isLogActive = true;
+
+					canAcceptInput = false;
+				}
 			}
 
 		}
@@ -74,6 +148,7 @@ void Game::Run()
 			if (timeRemaining <= 0.0f)
 			{
 				isPaused = true;
+				score = 0;
 				messageText.setString("Out of time!");
 				PositionText(messageText, posMessageX, posMessageY);
 			}
@@ -156,14 +231,59 @@ void Game::Run()
 			if (spriteCloud03.getPosition().x > SCREEN_X)
 				isCloud03Active = false;
 
+			//Update Score
 			std::stringstream ss;
 			ss << "Score = " << score;
 			scoreText.setString(ss.str());
+
+			for (int i = 0; i < NUM_BRANCHES; i++)
+			{
+				float height = i * 150;
+
+				if (branchPositions[i] == Side::LEFT)
+				{
+					branches[i].setPosition(610, height);
+					branches[i].setRotation(180);
+				}
+				else if (branchPositions[i] == Side::RIGHT)
+				{
+					branches[i].setPosition(1330, height);
+					branches[i].setRotation(0);
+				}
+				else
+				{
+					branches[i].setPosition(3000, height);
+				}
+			}
+
+			if (isLogActive)
+			{
+				spriteLog.setPosition(spriteLog.getPosition().x + (logSpeedX * dt.asSeconds()),
+					spriteLog.getPosition().y + (logSpeedY * dt.asSeconds()));
+				if (spriteLog.getPosition().x < -100 || spriteLog.getPosition().x > 2000)
+				{
+					isLogActive = false;
+					spriteLog.setPosition(logInitPosX, logInitPosY);
+				}
+			}
+
 		}
 
+		if (branchPositions[5] == playerSide)
+		{
+			isPaused = true;
+			canAcceptInput = false;
+			spriteRIP.setPosition(525, 760);
+			spritePlayer.setPosition(2000, 660);
+			messageText.setString("SQUISHED!");
+			PositionText(messageText, 1920 / 2, 1080 / 2);
+		}
+		
 		window->clear();
 		DrawSprites();
 		DrawText();
+		if (isPaused)
+			window->draw(messageText);
 		window->display();
 	}
 
@@ -219,10 +339,41 @@ void Game::DrawSprites()
 	window->draw(spriteTree);
 	window->draw(spriteBee);
 	window->draw(timeBar);
+	for (int i = 0; i < NUM_BRANCHES; i++)
+	{
+		window->draw(branches[i]);
+	}
+	window->draw(spritePlayer);
+	window->draw(spriteAxe);
+	window->draw(spriteRIP);
+	window->draw(spriteLog);
 }
 
 void Game::DrawText()
 {
-	window->draw(messageText);
 	window->draw(scoreText);
+}
+
+void Game::UpdateBranches(int seed)
+{
+	for (int i = NUM_BRANCHES - 1; i > 0; i--)
+	{
+		branchPositions[i] = branchPositions[i - 1];
+	}
+
+	srand((int)time(0) + seed);
+	int r = (rand() % 5);
+
+	switch (r)
+	{
+	case 0:
+		branchPositions[0] = Side::LEFT;
+		break;
+	case 1:
+		branchPositions[0] = Side::RIGHT;
+		break;
+	default:
+		branchPositions[0] = Side::NONE;
+		break;
+	}
 }
--- CMM_Timber/src/Program/Game.h
+++ CMM_Timber/src/Program/Game.h
@@ -1,5 +1,6 @@
 #pragma once
 #include <SFML/Graphics.hpp>
+enum class Side { LEFT, RIGHT, NONE };
 
 class Game
 {
@@ -25,6 +26,17 @@ private:
 		"res/assets/graphics/bee.png";
 	const std::string pathCloudGraphic =
 		"res/assets/graphics/cloud.png";
+	const std::string pathBranchGraphic =
+		"res/assets/graphics/branch.png";
+	const std::string pathPlayerGraphic =
+		"res/assets/graphics/player.png";
+	const std::string pathRIPGraphic = 
+		"res/assets/graphics/RIP.png";
+	const std::string pathAxeGraphic = 
+		"res/assets/graphics/axe.png";
+	const std::string pathLogGraphic = 
+		"res/assets/graphics/log.png";
+
 
 	const std::string pathFont =
 		"res/assets/fonts/FugazOne-Regular.ttf";
@@ -37,18 +49,20 @@ private:
 	sf::Clock dtClock;
 
 	bool isPaused = true;
+	bool canAcceptInput = false;
 
 	sf::RectangleShape timeBar;
 	float timeBarStartWidth = 400;
 	float timeBarHeight = 80;
 
 	sf::Time gameTimeTotal;
-	float timeRemaining = 6.0f;
+	const float INITIAL_TIME = 6.0f;
+	float timeRemaining = INITIAL_TIME;
 	float timeBarWidthPerSecond = timeBarStartWidth / timeRemaining;
 
 	sf::Font font;
 	sf::Text messageText;
-	int sizeWelcomeText = 75;
+	int sizeMessageText = 75;
 	float posMessageX = 1920 / 2;
 	float posMessageY = 1080 / 2;
 	std::string messageString =
@@ -84,6 +98,38 @@ private:
 	bool isCloud03Active = false;
 	float cloud03Speed = 0.0f;
 
+	sf::Texture textureBranch;
+	static const int NUM_BRANCHES = 6;
+	sf::Sprite branches[NUM_BRANCHES];
+	Side branchPositions[NUM_BRANCHES];
+
+	sf::Texture texturePlayer;
+	sf::Sprite spritePlayer;
+	Side playerSide = Side::LEFT;
+	int playerInitPosX = 600;
+	int playerInitPosY = 860;
+
+	sf::Texture textureRIP;
+	sf::Sprite spriteRIP;
+	int RIPInitPosX = 675;
+	int RIPInitPosY = 2000;
+
+	sf::Texture textureAxe;
+	sf::Sprite spriteAxe;
+	int axeInitPosX = 700;
+	int axeInitPosY = 830;
+	const float AXE_POSITION_LEFT = 700;
+	const float AXE_POSITION_RIGHT = 1075;
+
+	sf::Texture textureLog;
+	sf::Sprite spriteLog;
+	int logInitPosX = 810;
+	int logInitPosY = 720;
+	bool isLogActive = false;
+	float logSpeedX = 1000;
+	float logSpeedY = 1500;
+
+
 	void SetupSprite(std::string path, sf::Texture& texture,
 		float posX, float posY, sf::Sprite& spriteToSet);
 	void SetupText(std::string path, sf::Font& font,
@@ -93,6 +139,7 @@ private:
 
 	void DrawSprites();
 	void DrawText();
+	void UpdateBranches(int seed);
 
 };
 
